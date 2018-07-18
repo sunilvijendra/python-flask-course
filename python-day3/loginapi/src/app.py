6a9a340f51d8bd8b2655bdea5566300e769539bb
Sunil Vijendra <sunil.vijendra@gmail.com>
2018-07-18 12:36:58 +0530

    app.py changes

--- python-day3/loginapi/src/app.py
+++ python-day3/loginapi/src/app.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
 
-from loginapi.src.resources.Users import UsersResource
+from loginapi.src.resources.Users import Users
 
 app = Flask(__name__)
 api = Api(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
 #api.add_resource(User, '/user')
 
-api.add_resource(UsersResource, '/users')
+api.add_resource(Users, '/users')
 
 #api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
 
 
